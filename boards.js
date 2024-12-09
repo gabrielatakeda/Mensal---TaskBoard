cf6d868e6ec81c0e7d003864f23a39b947b8aedf
gabrielaifpr <gabrielatakedaa@gmail.com>
2024-12-09 13:49:54 -0300

    tarefas arrumado

--- boards.js
+++ boards.js
@@ -4,8 +4,26 @@ import { saveLocalStorage, getLocalStorage } from "./global.js";
 const dropdownButton = document.getElementById("dropdownButton");
 const dropdownMenu = document.getElementById("dropdownMenu");
 const columnsContainer = document.getElementById("columnsContainer"); // Verifique se isso está definido corretamente
+columnsContainer.innerHTML = '';
+// const taskContainer = document.getElementById("taskContainer-${column.Id}");
+
+// Função para fechar o dropdown ao clicar fora dele 
+window.addEventListener('click', function(event) { 
+    if (!dropdownButton.contains(event.target) && !dropdownMenu.contains(event.target)) { 
+        dropdownMenu.classList.remove('show'); 
+    } 
+});
+dropdownButton.addEventListener("click", function(event) {
+    event.stopPropagation();
+    dropdownMenu.classList.toggle("show");
+    if (dropdownMenu.classList.contains("show")) {
+        loadBoard();
+    }
+});
 
 async function loadBoard() {
+
+    dropdownMenu.innerHTML = '';
     try {
         const response = await fetch(`${API_BASE_URL}/Boards`);
 
@@ -34,22 +52,21 @@ async function loadBoard() {
             dropdownMenu.appendChild(boardLink);
         });
     } catch (error) {
-        console.error("Aconteceu um erro inesperado, tente novamente.");
+        console.error("Aconteceu um erro inesperado, tente novamente.boards", error);
     }
 }
 
 async function loadColumns(boardId) {
     const columnsContainer = document.getElementById("columnsContainer"); // Verifique se isso está definido corretamente
-
     try {
         const response = await fetch(`${API_BASE_URL}/ColumnByBoardId?BoardId=${boardId}`);
 
         if (!response.ok) {
             if (response.status === 422) {
                 const errorData = await response.json(); // Converte a resposta em JSON se o status for 422 (Unprocessable Entity)
-                alert(errorData.Errors[0]); // Exibe o primeiro erro da lista de erros recebida
+                console.error(errorData.Errors[0]);
             } else {
-                alert("Aconteceu um erro inesperado, tente novamente."); // Exibe um alerta de erro genérico se o status não for 422
+                console.error("Aconteceu um erro inesperado, tente novamente.");
                 return;
             }
         }
@@ -59,50 +76,108 @@ async function loadColumns(boardId) {
 
         listColumns.forEach(column => {
             const create = document.createElement("div"); // Cria um novo elemento <div> para cada coluna
-            create.className = "column"; // Adiciona uma classe para estilização
-            create.textContent = column.Name; // Define o texto visível com o nome da coluna
-            columnsContainer.appendChild(create); // Adiciona o elemento <div> ao container de colunas
+            // create.className = "column"; // Adiciona uma classe para estilização
+            // create.textContent = column.Name; // Define o texto visível com o nome da coluna
+            create.innerHTML = `
+                <div class="column">${column.Name}</h3>
+                <div class="taskContainer" id="taskContainer-${column.Id}"></div>`;
+            
+            columnsContainer.appendChild(create);
+            loadTasks(column.Id);
+            // columnsContainer.innerHTML = '';
+            // console.log("coluna criada");
         });
+
+
+        // document.addEventListener('DOMContentLoaded', function(columnId) {
+        //     loadTasks(column.Id);
+        //     console.log("puxar task");
+        // })
     } catch (error) {
-        console.error("Erro ao fazer a requisição:", error); // Loga o erro se a requisição falhar
+        console.error("Erro ao fazer a requisição:colunas", error); // Loga o erro se a requisição falhar
     } finally {
         console.log("Requisição para carregar colunas finalizada.");
     }
+    document.addEventListener('DOMContentLoaded', function(columnId) {
+        loadTasks(column.Id);
+        console.log("puxar task");
+    })
 }
 
-// Alternar entre os modos claro e escuro
-document.querySelectorAll('input[name="theme"]').forEach((elem) => {
-    elem.addEventListener("change", function(event) {
-        if (event.target.value === "dark") {
-            document.body.classList.add("dark-mode");
-            document.querySelector('.header').classList.add("dark-mode");
-            document.querySelector('.footer').classList.add("dark-mode");
-            localStorage.setItem('theme', 'dark');
-        } else {
-            document.body.classList.remove("dark-mode");
-            document.querySelector('.header').classList.remove("dark-mode");
-            document.querySelector('.footer').classList.remove("dark-mode");
-            localStorage.setItem('theme', 'light');
+
+
+
+async function loadTasks(columnId) {
+    console.log("carregando task");
+    const taskContainer = document.getElementById(`taskContainer-${columnId}`);
+    console.log("task container encontrado");
+
+    if (!taskContainer){
+        console.error("taskContainer nao encontrado");
+        return;
+    }
+    
+    try{
+        const response = await fetch(`${API_BASE_URL}/TasksByColumnId?ColumnId=${columnId}`);
+        console.log("task carregada da url")
+        if (!response.ok) {
+            if (response.status === 422) {
+                const errorData = await response.json(); // Converte a resposta em JSON se o status for 422 (Unprocessable Entity)
+                console.error(errorData.Errors[0]);
+            } else {
+                console.error("Aconteceu um erro inesperado, tente novamente.");
+                return;
+            }
         }
-    });
-});
+        
+        const listTask = await response.json(); // Converte a resposta em um array de objetos
+        // taskContainer.innerHTML = '';
+
+        listTask.forEach(task => {
+            const create = document.createElement("div"); // Cria um novo elemento <div> para cada coluna
+            create.className = "task"; // Adiciona uma classe para estilização
+            // create.textContent = task.Name; // Define o texto visível com o nome da coluna
+            create.innerHTML = `
+                <p><strong>${task.Title}</strong></p>
+                <p>${task.Description}</p>`;
+            taskContainer.appendChild(create); // Adiciona o elemento <div> ao container de colunas
+        })
+    } catch (error) {
+        console.error("Erro ao fazer a requisição: tarefa", error); // Loga o erro se a requisição falhar
+    } finally {
+        console.log("Requisição para carregar tarefas finalizada.");
+    }
+
+};
+// Alternar entre os modos claro e escuro
+// document.querySelectorAll('input[name="theme"]').forEach((elem) => {
+//     elem.addEventListener("change", function(event) {
+//         if (event.target.value === "dark") {
+//             document.body.classList.add("dark-mode");
+//             document.querySelector('.header').classList.add("dark-mode");
+//             document.querySelector('.footer').classList.add("dark-mode");
+//             localStorage.setItem('theme', 'dark');
+//         } else {
+//             document.body.classList.remove("dark-mode");
+//             document.querySelector('.header').classList.remove("dark-mode");
+//             document.querySelector('.footer').classList.remove("dark-mode");
+//             localStorage.setItem('theme', 'light');
+//         }
+//     });
+// });
 
 // Carregar a preferência do tema do localStorage ao carregar a página
-document.addEventListener('DOMContentLoaded', function() {
-    const savedTheme = localStorage.getItem('theme');
-    if (savedTheme && savedTheme === 'dark') {
-        document.body.classList.add('dark-mode');
-        document.querySelector('.header').classList.add('dark-mode');
-        document.getElementById('dark').checked = true;
-    } else {
-        document.getElementById('light').checked = true;
-    }
-});
+// document.addEventListener('DOMContentLoaded', function() {
+//     const savedTheme = localStorage.getItem('theme');
+//     if (savedTheme && savedTheme === 'dark') {
+//         document.body.classList.add('dark-mode');
+//         document.querySelector('.header').classList.add('dark-mode');
+//         document.querySelector('.columns').classList.add('dark-mode');
+//         document.querySelector('.dropdown-button').classList.add('dark-mode');
+//         document.getElementById('dark').checked = true;
+//     } else {
+//         document.getElementById('light').checked = true;
+//     }
+// });
 
-dropdownButton.addEventListener("click", function(event) {
-    event.stopPropagation();
-    dropdownMenu.classList.toggle("show");
-    if (dropdownMenu.classList.contains("show")) {
-        loadBoard();
-    }
-});
+
